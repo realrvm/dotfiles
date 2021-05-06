36438ffd3833ac3e758fb7134de4c9fb6be32cad
realrvm <romanmik@yandex.ru>
2021-05-06 06:02:32 +0300

    +skeletons

---
+++ vim/skeletons/react-typescript.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from "react";
+import styled from "styled-components";
+
+interface Props {}
+
+const NewComponent: FC<Props> = (props) => {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
+
+export default NewComponent;
